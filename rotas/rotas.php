f00d6cdfd95e3a83cefb069ac17eaea1538cf0e3
pedro <pk.bleite@gmail.com>
2023-10-04 23:05:58 -0300

    sistema inicial de rotas

---
+++ rotas/rotas.php
@@ -0,0 +1,31 @@
+function rotas() {
+    function erro() {
+        require("../controles/erro.php");
+        die();
+    }
+    function defineUrl()    {
+        $checaUrl = parse_url($_SERVER["REQUEST_URI"])["path"]; //verifica o URL com base na variavel global server.
+        return $checaUrl;
+    }
+    function defineRotas()  {
+        //cria uma array associativa, ou seja uma array que tem um nome, também chamada de hash.
+        $rotas = [
+            "/" => "/controles/principal.php/",
+            "/aluno" => "/controles/aluno.php/",
+            "/empresa" => "/controles/empresa.php/",
+            "/admin" => "/controles/adm.php/"
+        ]
+        return $rotas;
+    }
+    function checaUrl( $caminho, $rota ) {
+        if( array_key_exists($caminho, $rota ) )    {
+            require($rota[$caminho]);
+        }else{
+            erro();
+        }
+    }
+    $pegaValorRota = defineRotas();
+    $pegaValorUrl = defineUrl();
+    checaUrl( $pegaValorUrl, $pegaValorRota );
+
+}
