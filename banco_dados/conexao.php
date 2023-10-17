83fb88fd7d6a216865b8907540eb91df2a81b57b
pedro <pk.bleite@gmail.com>
2023-10-16 23:11:28 -0300

    msqli conectando normalmente com as sessao.

--- banco_dados/conexao.php
+++ banco_dados/conexao.php
@@ -1,10 +1,15 @@
 <?php
-    // session_Start();
+    //checa se a sessão já existe, caso não, cria uma.
+     if(!isset($_SESSION)) 
+     { 
+         session_start(); 
+     } 
     $host = "localhost";
     $usuario = "root";
     $senha = "";
     $banco = "id21376737_pedro";
 
+    //Estabelece conexão via msqli
     $conexao = mysqli_connect($host, $usuario, $senha, $banco);
     if( mysqli_connect_errno() ) {
         exit("Falha ao se conectar com o database" . mysqli_connect_error());
--- scripts/php/login_cadastro/cadastro.aluno.php
+++ scripts/php/login_cadastro/cadastro.aluno.php
@@ -3,11 +3,12 @@
  * @var mysqli $conexao
  */
     include_once("../../../banco_dados/conexao.php");
+    
     function preparaAlunoSignIn()   {
         require("../../../banco_dados/conexao.php");
-        if ($stmt = $conexao->prepare('SELECT id, password FROM cadastroAluno WHERE username = ?')) {
-            // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-            $stmt->bind_param('s', $_POST['username']);
+        if ($stmt = $conexao->prepare('SELECT idCadastroAluno, senhaRA FROM cadastroAluno WHERE nomeAluno = ?')) {
+            // Bind parameters (s = string, i = int, b = blob, etc), in our case the nomeAluno is a string so we use "s"
+            $stmt->bind_param('s', $_POST['nomeAluno']);
             $stmt->execute();
             // Store the result so we can check if the account exists in the database.
             $stmt->store_result();
